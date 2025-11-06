0a7ef7f81438dd5afe5efd4a5a3c97a868def3c2
Vladislav <vsergeev201530@gmail.com>
2025-11-06 11:01:20 +0500

    1293

---
+++ 1293.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    short a, b, n;
+
+    cin >> n >> a >> b;
+    cout << n * a * b * 2;
+}
